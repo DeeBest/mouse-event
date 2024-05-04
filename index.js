5d9716f1fd7e99048011855bdee4700b8754a664
DeeBest <simphiwedladla8@gmail.com>
2024-05-04 13:35:40 +0200

    added event listener on the window

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const yPositionCounter = document.getElementById('y-position-counter');
+const xPositionCounter = document.getElementById('x-position-counter');
+
+window.addEventListener('mousemove', (e) => {
+  yPositionCounter.textContent = e.offsetY;
+  xPositionCounter.textContent = e.offsetX;
+});
