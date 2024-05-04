695322c7fa8a508c791157f5a562717d132c83ac
DeeBest <simphiwedladla8@gmail.com>
2024-05-04 13:20:49 +0200

    applied styling

--- index.js
+++ index.js
@@ -2,12 +2,3 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
-
-const backgroundImage = document.getElementById('bg-image-container');
-
-window.addEventListener('scroll', updateImage);
-
-function updateImage() {
-  backgroundImage.style.opacity = 1 - window.scrollY / 600;
-  backgroundImage.style.backgroundSize = 120 - window.scrollY / 20 + '%';
-}
